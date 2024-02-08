de8edc2826a2ed361fe788b85dec33def6fa568c
tomagon <agonnoudetom@gmail.com>
2024-02-08 12:30:23 +0100

    Fin du TP2

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ TPs du Cours d'indexation Web
 
 Par l'étudiant AGONNOUDE Tom
 
-## Crawler 
+Ce repo regroupes les différents TP ci dessous :
+
+1. [TP1 - Crawler](#tp1---crawler)
+2. [TP2 - Création d'index](#tp2---index-creation)
+
+
+## TP1 - Crawler 
 
 ### Exécution simple
 Pour exécuter le crawler:
@@ -26,3 +32,28 @@ Le temps d'attente entre deux téléchargements de pages est de 5s entre chaque 
 Pour le lancement de l'exécution du ficher main.py, on peut ajouter les arguments suivants: 
 - `sitemaps` : pour inclure la recherche les sitemaps et y récupérer des liens. La limite de nombre maximum de document à récupérer au total passe à 60 pages. On se limitera aussi à 15 liens par sitemap visitées.
 - `database` : pour créer une base de données relationnelle pour stocker les pages web trouvées ainsi que leur dates et heures de parsing respectifs. Les liens retournés dans le fichier final `crawled_webpages.txt` sont ceux enrégistrés dans la base de données avec leur âge.
+
+
+
+## TP2 - Index Creation  
+
+### Exécution simple
+Pour exécuter le code pour la création d'index :
+- Cloner le repo et se mettre dans le dossier `index_tp2/`
+- S'assurer que toutes les dépendances sont installés
+- Lancer l'exécution du fichier main.py
+
+```
+git clone https://github.com/tommy-21/projet-indexation-web.git && cd projet-indexation-web/index_tp2/
+pip install -r requirements.txt
+python main.py
+```
+Cette dernière commande permet de créer l'index non positionnel du titre le plus simple (renvoyé dans un fichier `title.non_pos_index.json`) avec comme traitement un downcasing et une tokenisation simple. Elle permet aussi de générer un fichier `metadata.json` contenant des statistiques sur les documents traités. Les autres exécutions possibles (décrites ci-dessous) produisent à minima ces deux fichiers. 
+
+### Arguments
+Le lancement de l'exécution du ficher main.py peut se faire suivi des arguments suivants: 
+- `stemming` : pour inclure du stemming dans le traitement des données. Renvoie le fichier `mon_stemmer.title.non_pos_index.json`.
+- `positional` : pour créer un index positionnel. Renvoie le fichier `title.pos_index.json`. Mets un peu de temps (plus de 5 min)
+- Mettre les deux arguments précédents à la fois renverra en plus un quatrième index, index positionnel obtenu à partir des données stemmé et renvoyé dans le fichier `mon_stemmer.title.pos_index.json` (tout ça est un peu chronophage par contre...)
+
+Tout les index sont faits avec les titres.
--- index_tp2/main.py
+++ index_tp2/main.py
@@ -47,16 +47,47 @@ def main(stemming=False, positional=False):
 
     # Prétraitement des documents
     titles = [url["title"] for url in crawled_urls]
-    processed_data = [{"id":titles.index(title)+1, "data":processing(title)} for title in titles]
+    h1s = [url["h1"] for url in crawled_urls]
+    contents = [url["content"] for url in crawled_urls]
+
+    processed_titles = [{"id":titles.index(title)+1, "data":processing(title)} for title in titles]
     if stemming:
-        stemmed_data = [{"id":titles.index(title)+1, "data":processing(title, stemming=stemming)} for title in titles]
+        stemmed_titles = [{"id":titles.index(title)+1, "data":processing(title, stemming=stemming)} for title in titles]
+    processed_h1s = [{"id":h1s.index(h1)+1, "data":processing(h1)} for h1 in h1s]
+    processed_contents = [{"id":contents.index(content)+1, "data":processing(content)} for content in contents]
+
 
     # Statistiques sur les documents
+    nb_docs = len(crawled_urls)
+    nb_tok_titles = sum([len(doc["data"]) for doc in processed_titles])
+    nb_tok_h1s = sum([len(doc["data"]) for doc in processed_h1s])
+    nb_tok_contents = sum([len(doc["data"]) for doc in processed_contents])
+    nb_tokens = nb_tok_titles+nb_tok_h1s+nb_tok_contents
+
+    statistiques = {
+        "Nombre de documents" : nb_docs, 
+        "Nombre total de tokens" : nb_tokens,
+        "Moyenne de tokens par documents" : nb_tokens/nb_docs,
+        "Titre" : {
+            "Nombre de tokens" : nb_tok_titles,
+            "Nombres moyen de tokens par documents" : nb_tok_titles/nb_docs
+        },
+        "h1" : {
+            "Nombre de tokens" : nb_tok_h1s,
+            "Nombres moyen de tokens par documents" : nb_tok_h1s/nb_docs,
+        },
+        "content" : {
+            "Nombre de tokens" : nb_tok_contents,
+            "Nombres moyen de tokens par documents" : nb_tok_contents/nb_docs,
+        }
+    }
+    # Sauvegarde des métadonnées dans un fichier json
+    with open("metadata.json", 'w') as file:
+        json.dump(statistiques, file, indent=4)
+        print("Metadata saved!")
     
 
-
-
-    non_pos_index = create_index(prepared_data=processed_data)
+    non_pos_index = create_index(prepared_data=processed_titles)
     # Sauvegarde de l'index dans un fichier json
     file_name = 'title.non_pos_index.json'
     with open(file_name, 'w') as file:
@@ -64,7 +95,7 @@ def main(stemming=False, positional=False):
         print("Non positional index saved!")
 
     if stemming:
-        stemmed_non_pos_index = create_index(prepared_data=stemmed_data)
+        stemmed_non_pos_index = create_index(prepared_data=stemmed_titles)
         # Sauvegarde de l'index dans un fichier json
         file_name = 'mon_stemmer.title.non_pos_index.json'
         with open(file_name, 'w') as file:
@@ -72,7 +103,7 @@ def main(stemming=False, positional=False):
             print("Stemmed non positional index saved!")
 
     if positional:
-        pos_index = create_index(prepared_data=processed_data, positional=True)
+        pos_index = create_index(prepared_data=processed_titles, positional=True)
         # Sauvegarde de l'index dans un fichier json
         file_name = 'title.pos_index.json'
         with open(file_name, 'w') as file:
@@ -80,7 +111,7 @@ def main(stemming=False, positional=False):
             print("Positional index saved!")
 
     if stemming and positional:
-        stemmed_pos_index = create_index(prepared_data=processed_data, positional=True)
+        stemmed_pos_index = create_index(prepared_data=processed_titles, positional=True)
         # Sauvegarde de l'index dans un fichier json
         file_name = 'mon_stemmer.title.pos_index.json'
         with open(file_name, 'w') as file:
