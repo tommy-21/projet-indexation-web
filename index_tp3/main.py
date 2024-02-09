07be290718cd3bd0fbc649576f2f5e9fef582c86
tomagon <agonnoudetom@gmail.com>
2024-02-09 18:11:30 +0100

    Fin tp3, version sans bonus

--- index_tp3/main.py
+++ index_tp3/main.py
@@ -108,27 +108,26 @@ def main():
 
     # Demander à l'utilisateur d'entrer une requête
     requete = input("Veuillez entrer votre requête : ")
-
     resultats = {}
     resultats["Requete"] = requete
-    nom_fichier_resultat = f'resultats_{requete}'
 
     # traitement de la requête
     requete = requete.lower().split()
 
+    # Recherche des document avec tout les tokens de la requếte
+    tokens_contents = [set(content_index[tok].keys()) for tok in requete] if sum([tok not in content_index.keys() for tok in requete]) == 0 else []
+    tokens_titles = [set(title_index[tok].keys()) for tok in requete] if sum([tok not in title_index.keys() for tok in requete]) == 0 else []
 
-
-    tokens_contents = [set(content_index[tok].keys()) for tok in requete if tok in content_index.keys()]
-    tokens_titles = [set(title_index[tok].keys()) for tok in requete if tok in title_index.keys()]
-
-    if len(requete)!=len(tokens_contents) and len(requete)!=len(tokens_titles): 
+    if tokens_contents==[] and tokens_titles==[]:
         documents_restants = None
     else:
+        title_has_all_tokens = set()
         if tokens_titles:
             title_has_all_tokens = tokens_titles[0]
             for tok_list in tokens_titles:
                 title_has_all_tokens = title_has_all_tokens & tok_list
 
+        content_has_all_tokens = set()
         if tokens_contents:
             content_has_all_tokens = tokens_contents[1]
             for tok_list in tokens_contents:
@@ -139,27 +138,18 @@ def main():
         documents_restants += [("content", docid) for docid in content_has_all_tokens - title_has_all_tokens]
 
 
-    # importance de la partie 
+    # Calcul des features des documents:
     features_documents = dict()
     if documents_restants:
         for docs in documents_restants:
             docID = docs[1]
             features_documents[docID] = {}
-
-            features_documents[docID]["title"] = 0
-            features_documents[docID]["content"] = 0
             features_documents[docID]["Occurrence_title"] = 0
             features_documents[docID]["Occurrence_content"] = 0
             features_documents[docID]["Proximite_title"] = 0
             features_documents[docID]["Proximite_content"] = 0
 
-            # contenu ou titre
-            if docs[0] == "title" or docs[0] == "both":
-                features_documents[docID]["title"] = 1
-            if docs[0] == "content" or docs[0] == "both":
-                features_documents[docID]["content"] = 1
-
-            # Nombres d'apparition
+            # Nombres d'apparition ----------------------------
             features_documents[docID]["Occurrence_title"] = 0 
             features_documents[docID]["Occurrence_content"] = 0
             if docs[0] == "title" or docs[0] == "both":
@@ -167,18 +157,20 @@ def main():
             if docs[0] == "content" or docs[0] == "both":
                 features_documents[docID]["Occurrence_content"] = sum([content_index[tok][docID]["count"] for tok in requete if tok in content_index.keys()])
             
-            # Proximité
+            # Proximité ---------------------------------------
             positions_tokens_title = {}
             positions_tokens_content = {}
 
             if docs[0] == "title" or docs[0] == "both":
                 for token in requete:
-                    positions_tokens_title[token] = title_index[token][docID]["positions"] 
+                    if token in title_index:
+                        positions_tokens_title[token] = title_index[token][docID]["positions"] 
                 features_documents[docID]["Proximite_title"] = calculer_proximite(requete, positions_tokens_title)
                 
             if docs[0] == "content" or docs[0] == "both":
                 for token in requete:
-                    positions_tokens_content[token] = content_index[token][docID]["positions"]
+                    if token in content_index:
+                        positions_tokens_content[token] = content_index[token][docID]["positions"]
                 features_documents[docID]["Proximite_content"] = calculer_proximite(requete, positions_tokens_content)
 
             # # TF et IDF
@@ -187,29 +179,33 @@ def main():
             # features_documents[docID]["IDF"] = sum([idf_title[tok] for tok in requete]) \
             #                                      + sum([tf_content[tok] for tok in requete])
 
-    # Calcul des scores
-    scores = {}
-    for docid in features_documents.keys():
-        scores[docid] = 5*features_documents[docid]["Occurrence_title"] \
-            + features_documents[docid]["Occurrence_content"] - 0.1*features_documents[docid]["Proximite_title"] - 0.5*features_documents[docid]["Proximite_content"]
+        # Calcul des scores
+        scores = {}
+        for docid in features_documents.keys():
+            scores[docid] = 5*features_documents[docid]["Occurrence_title"] + features_documents[docid]["Occurrence_content"] \
+                            - 0.1*features_documents[docid]["Proximite_title"] - 0.5*features_documents[docid]["Proximite_content"]
+            
         
+        # Récupération des documents et préparation des résultats
+        liste_id_docs_pertinents = sorted(scores, key=scores.get, reverse=True)
+        documents_df = pd.DataFrame(documents, dtype=str)
+        documents_df = documents_df.set_index("id")
+        docs_pertinents = [{"Id":docid,
+                            "Titre":documents_df.loc[docid, "title"], 
+                            "Url":documents_df.loc[docid, "url"]} for docid in liste_id_docs_pertinents]
+        
+        resultats["Documents par ordre de pertinence"] = docs_pertinents
+        resultats["Nombre total de documents"] = len(documents)
+        resultats["Nombre de documents ayant survécu au filtre"] = len(liste_id_docs_pertinents)
 
-    liste_id_docs_pertinents = sorted(scores, key=scores.get, reverse=True)
-    documents_df = pd.DataFrame(documents, dtype=str)
-    documents_df = documents_df.set_index("id")
-    docs_pertinents = [{"Id":docid,
-                        "Titre":documents_df.loc[docid, "title"], 
-                        "Url":documents_df.loc[docid, "url"]} for docid in liste_id_docs_pertinents]
-    
-    resultats["Documents par ordre de pertinence"] = docs_pertinents
-    resultats["Nombre total de documents"] = len(documents)
-    resultats["Nombre de documents ayant survécu au filtre"] = len(liste_id_docs_pertinents)
-    
-    with open(nom_fichier_resultat, 'w') as file:
-        json.dump(resultats, file, indent=4)
-        print("Results saved!")
-        
-    print(resultats)
+        with open("results.json", 'w') as file:
+            json.dump(resultats, file, indent=4)
+            print("\nResults saved!")
+            
+        print(resultats)
+
+    else:
+        print("\nAucun document ne contient tous les mots de la requête")
 
 if __name__ == '__main__':
     main()
